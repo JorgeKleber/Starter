c217685d717ece3ad6d89985d8e620cdfd163459
Jorge Kleber <jorge_kleber@outlook.com>
2022-09-26 23:30:42 -0400

    Criação da página de login
    - Criação da ViewModelBase, classe base das ViewModels.
    - Criação da página de Login e binding com a ViewModel.
    - Modificação da página inicial do projeto.

---
+++ Athena/Athena/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Athena.ViewModels
+{
+	public class LoginPageViewModel : ViewModelBase
+	{
+		private string _TitlePage;
+
+		public string TitlePage
+		{
+			get
+			{
+				return _TitlePage;
+			}
+			set
+			{
+				_TitlePage = value;
+				Notify(_TitlePage);
+			}
+		}
+
+		public LoginPageViewModel()
+		{
+			TitlePage = "Bem vindo ao Athenas!";
+		}
+	}
+}
